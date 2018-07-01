bb627090112cf3e4d5d4fd08a1acdc02ddd5a1ce
Pierre MARTIN <pierre.martin49@gmail.com>
2018-07-01 13:09:14 +0200

    Initial commit

---
+++ index.tsx
@@ -0,0 +1,20 @@
+import { Button } from '@material-ui/core';
+import * as React from 'react';
+import { render } from 'react-dom';
+import './styles.scss';
+
+export interface HelloMessageProps {
+  toto?: string;
+}
+
+class HelloMessage extends React.Component<HelloMessageProps> {
+  render() {
+    return (
+      <Button variant="contained" color="primary">
+        Hello {this.props.toto}
+      </Button>
+    );
+  }
+}
+
+render(<HelloMessage toto="Jane" />, document.getElementById('app'));
